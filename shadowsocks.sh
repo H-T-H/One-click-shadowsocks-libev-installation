819669f5fc6711eac9fc260ee09651dd8b80eb09
H-T-H <67087346+H-T-H@users.noreply.github.com>
2023-11-28 17:04:01 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -6,34 +6,43 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# 更新系统
+# 更新系统安装snap
 apt update
 apt upgrade -y
+sudo apt install snapd -y
+sudo snap install core
 
-# 安装依赖
-apt-get install -y git gettext build-essential unzip gzip python python-dev python-setuptools curl openssl libssl-dev autoconf automake libtool gcc make perl cpio libpcre3 libpcre3-dev zlib1g-dev libev-dev libc-ares-dev git qrencode
+# 安装shadowsocks libev
+sudo snap install shadowsocks-libev
 
-
-# 下载并安装Shadowsocks-libev
-git clone https://github.com/shadowsocks/shadowsocks-libev.git
-cd shadowsocks-libev
-git submodule update --init --recursive
-./autogen.sh
-./configure
-make && make install
-
-# 创建配置文件
-cp ./debian/shadowsocks-libev.init /etc/init.d/shadowsocks-libev
-chmod +x /etc/init.d/shadowsocks-libev
-cp ./debian/shadowsocks-libev.default /etc/default/shadowsocks-libev
-
-# 创建Shadowsocks-libev配置文件
-mkdir -p /etc/shadowsocks-libev
-cp ./debian/config.json /etc/shadowsocks-libev/config.json
-# 启动Shadowsocks-libev服务
-/etc/init.d/shadowsocks-libev start
+#写入配置文件
+cat > /snap/bin/config.json <<EOL
+{
+  "server": "0.0.0.0",
+  "server_port": 1,
+  "password": "teddysun.com",
+  "method": "aes-256-gcm",
+  "timeout": 600,
+  "no_delay": true,
+  "mode": "tcp_only"
+}
+EOL
 
 # 设置开机自启动
-update-rc.d shadowsocks-libev defaults
+cat > /etc/systemd/system/ss.service <<EOL
+[Unit]
+Description=Shadowsocks Server
+After=network.target
+
+[Service]
+Restart=on-abnormal
+ExecStart=/snap/bin/shadowsocks-libev.ss-server -c /snap/bin/config.json
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+
+
 
 echo "Shadowsocks-libev安装完成，配置文件位于 /etc/shadowsocks-libev/config.json"
